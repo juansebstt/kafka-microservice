df2a429d3d2d2ba53fd126b7d1834f5b2d868041
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-14 14:18:37 -0500

    Created GameEligibleEvent

--- src/main/java/com/eligibilitymicroservice/common/GameEligibleEvent.java
+++ src/main/java/com/eligibilitymicroservice/common/GameEligibleEvent.java
@@ -1,4 +1,19 @@
 package com.eligibilitymicroservice.common;
 
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class GameEligibleEvent {
+
+    private Integer id;
+    private String name;
+    private String userId;
+    private boolean isEligible;
+
 }
